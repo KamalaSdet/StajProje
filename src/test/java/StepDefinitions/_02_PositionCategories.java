9164275d286123d67327dd0cfec7ed39de662d0b
Palaque92 <boncukomer91@gmail.com>
2022-12-17 11:37:19 +0200

    Omerin ilk us

---
+++ src/test/java/StepDefinitions/_02_PositionCategories.java
@@ -0,0 +1,44 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Pages.LeftNav;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _02_PositionCategories {
+    DialogContent dc = new DialogContent();
+
+    LeftNav ln = new LeftNav();
+
+    @And("Naviagate to Position Categories")
+    public void naviagateToPositionCategories() {
+        ln.findAndClick("humanRsrButton");
+        ln.findAndClick("setupButtonUnderHuman");
+        ln.findAndClick("positionCategories");
+    }
+
+    @When("User add name in Position Categories")
+    public void userAddNameInPositionCategories(String name) {
+        dc.findAndClick("addButton");
+        dc.findAndSend("nameInput",name);
+        dc.findAndClick("saveButton");
+    }
+
+     @Then("Success message should be displayed")
+     public void successMessageShouldBeDisplayed() {
+         dc.findAndContainsText("successMessage","success");
+     }
+
+    @When("User add {string} in Position Categories")
+    public void userAddInPositionCategories(String name) {
+        dc.findAndClick("addButton");
+        dc.findAndSend("nameInput",name);
+        dc.findAndClick("saveButton");
+    }
+
+    @When("User delete the {string}")
+    public void userDeleteThe(String name) {
+        dc.findAndDelete(name);
+    }
+}
